4dc8accbe0feaa2412eadcfe756b5c453a625a9a
alexbsec <j.a.c.buschinelli@gmail.com>
2025-01-18 12:48:38 -0300

    window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,55 @@
 #include <cstdlib>
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 #include <print>
-#include "include/test.hpp"
 
 int main() {
-  std::println("Hi mom");
-  std::println("Test int is: {}", myInt);
+  if (!glfwInit()) {
+    std::println("Failed to initialize GLFW");
+    return EXIT_FAILURE;
+  }
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+  if (!window) {
+    std::println("Failed to instantiate window object");
+    return EXIT_FAILURE;
+  }
+
+  VkInstance instance;
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+  vkCreateInstance(&createInfo, nullptr, &instance);
+
+  VkSurfaceKHR surface;
+  glfwCreateWindowSurface(instance, window, nullptr, &surface);
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+  std::println("{} extension supported", extensionCount);
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  vkDestroySurfaceKHR(instance, surface, nullptr);
+  vkDestroyInstance(instance, nullptr);
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return EXIT_SUCCESS;
 }
